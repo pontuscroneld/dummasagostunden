88c59607095fe0f81be8d6a006a3e9fbd68e848b
pontuscroneld <pontuscroneld@gmail.com>
2020-09-23 22:44:19 +0200

    Döpt om filer till engelska, clearat lite i VC

--- dummasagostunden/ViewControllers/StartView.swift
+++ dummasagostunden/ViewControllers/StartView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class StartView: UIViewController {
     
@@ -29,14 +30,6 @@ class StartView: UIViewController {
     
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
