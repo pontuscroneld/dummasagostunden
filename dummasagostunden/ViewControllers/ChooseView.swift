91e36b8578eed91096d7fd73b631c4e6821cfdc6
pontuscroneld <pontuscroneld@gmail.com>
2020-09-24 12:29:57 +0200

    Added choose page

---
+++ dummasagostunden/ViewControllers/ChooseView.swift
@@ -0,0 +1,29 @@
+//
+//  ChooseView.swift
+//  dummasagostunden
+//
+//  Created by Pontus Croneld on 2020-09-24.
+//
+
+import UIKit
+
+class ChooseView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
