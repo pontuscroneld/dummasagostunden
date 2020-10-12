4d7fbd47c6644c9f9bf90800a3998db4a332f78a
pontuscroneld <pontuscroneld@gmail.com>
2020-10-12 10:19:06 +0200

    Gjort småpill

--- dummasagostunden/ViewControllers/ReadView.swift
+++ dummasagostunden/ViewControllers/ReadView.swift
@@ -55,8 +55,8 @@ class ReadView: UIViewController {
 
     
     @IBAction func startover(_ sender: Any) {
-        //performSegue(withIdentifier: "startover", sender: nil)
-        spk.stopSpeaking(at: AVSpeechBoundary.immediate)
+        
+       spk.stopSpeaking(at: AVSpeechBoundary.immediate)
         
         presentingViewController?.presentingViewController?.presentingViewController?.dismiss(animated: false, completion: nil)
         
